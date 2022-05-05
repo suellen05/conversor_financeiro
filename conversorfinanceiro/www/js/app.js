985241099f853266df3512851dea94f0ebfab603
Suellen <73621600+suellen05@users.noreply.github.com>
2022-05-04 22:08:25 -0300

    Atualizações conversor financeiro

--- conversorfinanceiro/www/js/app.js
+++ conversorfinanceiro/www/js/app.js
@@ -1,7 +1,19 @@
+
 function ConverterRealDolar(){
     let reais = document.querySelector('#reais').value 
-    let dolar = 4.92
-    valorConvertido = reais / dolar
-    document.querySelector('#dolares').innerHTML = valorConvertido
-}
+    let dolar = 0
 
+    let api = 'https://economia.awesomeapi.com.br/last/USD'
+    
+    $.ajax({
+        type:"GET",
+        url: api,
+        success: (function(retorno){
+        dolar = retorno.USDBRL.ask
+        valorConvertido = reais / dolar
+        document.querySelector('#dolares').innerHTML = valorConvertido
+
+   })
+})
+    
+}
